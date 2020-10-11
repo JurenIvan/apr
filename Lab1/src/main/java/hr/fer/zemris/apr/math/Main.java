b916e3193484ee7e55f46b6428737a9367372155
JurenIvan <juren.ivan@gmail.com>
2020-10-11 13:58:43 +0200

    remove useless comments from main

--- Lab1/src/main/java/hr/fer/zemris/apr/math/Main.java
+++ Lab1/src/main/java/hr/fer/zemris/apr/math/Main.java
@@ -3,19 +3,5 @@ package hr.fer.zemris.apr.math;
 public class Main {
     public static void main(String[] args) {
 
-
-//        var m2 = Matrix.parseString("4 10 5 -9 | -4 6 6 3 | -8 4 2 6 | -4 8 1 -3");
-//        System.out.println(m2);
-//
-//        var t2 = m2.nDecompose(LUP_DECOMPOSITION);
-//        System.out.println("LU");
-//        System.out.println(t2.getLUMatrix());
-//        System.out.println("L");
-//        System.out.println(t2.getLMatrix());
-//        System.out.println("U");
-//        System.out.println(t2.getUMatrix());
-//        System.out.println("P");
-//        System.out.println(t2.getPMatrix());
-
     }
 }
