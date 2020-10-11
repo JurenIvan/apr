d94c6ca412e68194c3d6d8a342151f390b74d422
JurenIvan <juren.ivan@gmail.com>
2020-10-12 00:51:09 +0200

    add demo example tests

--- Lab1/src/test/java/hr/fer/zemris/apr/math/Demo.java
+++ Lab1/src/test/java/hr/fer/zemris/apr/math/Demo.java
@@ -1,43 +1,100 @@
 package hr.fer.zemris.apr.math;
 
-import hr.fer.zemris.apr.math.matrix.IMatrix;
 import hr.fer.zemris.apr.math.matrix.Matrix;
-import hr.fer.zemris.apr.math.matrix.inverse.InverseStrategies;
-import hr.fer.zemris.apr.math.vector.IVector;
 import hr.fer.zemris.apr.math.vector.Vector;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static hr.fer.zemris.apr.math.matrix.determinant.DeterminantStrategies.TRIANGULAR_MATRICES;
+import static hr.fer.zemris.apr.math.matrix.inverse.InverseStrategies.LUP_DECOMPOSITION_INVERSE;
+import static hr.fer.zemris.apr.math.matrix.solver.EquasionSolvers.LUP_EQUATION_SOLVER;
+import static hr.fer.zemris.apr.math.matrix.solver.EquasionSolvers.LU_EQUATION_SOLVER;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Demo {
 
     @Test
-    public void demonstration() {
-        IVector v1 = Vector.parseSimple("2 3 -4").nAdd(Vector.parseSimple("-1 4 -3"));
-        assertEquals(Vector.parseSimple("1 7 -7"), v1);
+    public void zadatak01() {
+        var matrix = Matrix.parseString("5 2.22");
 
-        double s = v1.scalarProduct(Vector.parseSimple("-1 4 -3"));
-        assertEquals(48, s);
+        assertNotEquals("1.6666666666666666", String.valueOf(matrix.get(0, 0) / 3).substring(0, 18));
+    }
 
-        IVector v2 = v1.nVectorProduct(Vector.parseSimple("2 2 4"));
-        assertEquals(Vector.parseSimple("42 -18 -12"), v2);
+    @Test
+    @Disabled
+    public void zadatak02() {
+        var matrix = Matrix.parseString("3 9 6 | 4 12 12 | 1 -1 -1");
+        var right = Vector.parseSimple("12 12 1");
 
-        IVector v3 = v2.copy().normalize();
-        assertEquals(0.889, v3.get(0), 1e-3);
-        assertEquals(-0.381, v3.get(1), 1e-3);
-        assertEquals(-0.254, v3.get(2), 1e-3);
+        var expectedSolution = Vector.parseSimple("3 1 -1");
+        assertEquals(expectedSolution, matrix.solveSystem(LUP_EQUATION_SOLVER, right));
+        assertEquals(expectedSolution, matrix.solveSystem(LU_EQUATION_SOLVER, right));
+    }
 
-        IVector v4 = v2.scalarMultiply(-1);
-        assertEquals(-42, v4.get(0), 1e-3);
-        assertEquals(18, v4.get(1), 1e-3);
-        assertEquals(12, v4.get(2), 1e-3);
+    @Test
+    @Disabled
+    public void zadatak03() {
+        var matrix = Matrix.parseString("1 2 3 | 4 5 6 | 7 8 9");
+        var right = Vector.parseSimple("8 20 32");
 
-        IMatrix m1 = Matrix.parseString("1 2 3 |2 1 3| 4 5 1").nAdd(Matrix.parseString("-1 2 -3 |5 -2 7|-4 -1 3"));
-        IMatrix m2 = Matrix.parseString("1 2 3 |2 1 3| 4 5 1").nMultiply(Matrix.parseString("-1 2 -3 |5 -2 7|-4 -1 3").nTranspose(false));
-        IMatrix m3 = Matrix.parseString("-24 18 5 | 20 -15 -4 |-5 4 1").inverse(InverseStrategies.SUBVIEW_INVERSE).nMultiply(Matrix.parseString("1 2 3 | 0 1 4 | 5 6 0").inverse(InverseStrategies.SUBVIEW_INVERSE));
 
-        //   assertEquals(Matrix.parseString("0 4 0 |7 -1 10| 0 4 4"), m1);
-        //   assertEquals(Matrix.parseString("-6 22  3  |-9 29  0  |3  17  -18"), m2);
-        //   assertEquals(Matrix.parseString("1 0 0 |0 1 0| 0 0 1"), m3);
+        var expectedSolution = Vector.parseSimple("1 2 1");
+        assertEquals(expectedSolution, matrix.solveSystem(LUP_EQUATION_SOLVER, right));
+        assertEquals(expectedSolution, matrix.solveSystem(LU_EQUATION_SOLVER, right));
+    }
+
+    @Test
+    @Disabled
+    public void zadatak04() {
+        var matrix = Matrix.parseString("0.000001 3000000 2000000|1000000 2000000 3000000|2000000 1000000 2000000");
+        var right = Vector.parseSimple("12000000.000001 14000000 10000000");
+
+
+        var expectedSolution = Vector.parseSimple("1 2 3");
+        assertEquals(expectedSolution, matrix.solveSystem(LUP_EQUATION_SOLVER, right));
+        assertEquals(expectedSolution, matrix.solveSystem(LU_EQUATION_SOLVER, right));
+    }
+
+    @Test
+    public void zadatak05() {
+
+    }
+
+    @Test
+    public void zadatak06() {
+        var matrix = Matrix.parseString("4000000000 1000000000 3000000000 | 4 2 7 | 0.0000000003 0.0000000005 0.0000000002");
+        var right = Vector.parseSimple("9000000000 15 0.0000000015");
+
+        var result = matrix.solveSystem(LUP_EQUATION_SOLVER, right);
+        assertTrue(Vector.parseSimple("1 2 1").equals(result, 6));
+    }
+
+    @Test
+    @Disabled
+    public void zadatak07() {
+        var matrix = Matrix.parseString("1 2 3|4 5 6|7 8 9");
+
+        assertThrows(Exception.class, () -> matrix.inverse(LUP_DECOMPOSITION_INVERSE));
+    }
+
+    @Test
+    public void zadatak08() {
+        var matrix = Matrix.parseString("4 -5 -2|5 -6 -2|-8 9 3");
+        var expected = Matrix.parseString("0 -3 -2 | 1 -4 -2| -3 4 1");
+        assertEquals(expected, matrix.inverse(LUP_DECOMPOSITION_INVERSE));
+    }
+
+    @Test
+    public void zadatak09() {
+        var matrix = Matrix.parseString("4 -5 -2|5 -6 -2|-8 9 3");
+
+        assertEquals(1, matrix.determinant(TRIANGULAR_MATRICES));
+    }
+
+    @Test
+    public void zadatak10() {
+        var matrix = Matrix.parseString("3 9 6|4 12 12 |1 -1 1");
+
+        assertEquals(48, matrix.determinant(TRIANGULAR_MATRICES));
     }
 }
