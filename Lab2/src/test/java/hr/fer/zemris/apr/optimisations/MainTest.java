7c059bd03cae49ec97b71c74e393a8b6d518e2a0
JurenIvan <juren.ivan@gmail.com>
2020-10-26 19:26:32 +0100

    add base for 2nd lab exercise

---
+++ Lab2/src/test/java/hr/fer/zemris/apr/optimisations/MainTest.java
@@ -0,0 +1,12 @@
+package hr.fer.zemris.apr.optimisations;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void main() {
+        Assertions.assertEquals(5, 2 + 3);
+    }
+}
